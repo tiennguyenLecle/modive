b87e4a3d4e04f43057935e08c93ec9360fd42759
tiennguyen <tiennguyen@lecle.co.kr>
2025-07-20 22:42:16 +0700

    feat: config locale en and kr

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,6 +1,6 @@
 export const i18n = {
-  defaultLocale: 'vi',
-  locales: ['vi', 'en'],
+  defaultLocale: 'en',
+  locales: ['en', 'kr'],
 } as const;
 
 export type Locale = (typeof i18n)['locales'][number];
---
+++ src/lib/dictionary.ts
@@ -0,0 +1,14 @@
+import 'server-only';
+import type { Locale } from '@/../i18n.config';
+
+const dictionaries = {
+  en: () => import('@/dictionaries/en.json').then(module => module.default),
+  kr: () => import('@/dictionaries/kr.json').then(module => module.default),
+};
+
+export const getDictionary = async (locale: Locale) => {
+  if (locale === 'kr') {
+    return dictionaries.kr();
+  }
+  return dictionaries.en();
+};

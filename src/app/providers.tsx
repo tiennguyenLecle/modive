c4b408ff0561ca4440d3c5515b4fdf68b3a31c14
diemnguyen <diemnguyen@lecle.co.kr>
2025-09-03 13:00:00 +0900

    feat: progress bar - nav

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,9 @@
 'use client';
 
+import { useEffect } from 'react';
 import { AbstractIntlMessages, NextIntlClientProvider } from 'next-intl';
+import { usePathname } from 'next/navigation';
+import NProgress from 'nprogress';
 
 import { ThemeRegistry } from '@/lib/theme-registry';
 
@@ -10,11 +13,21 @@ type Props = {
   locale: string;
 };
 
+NProgress.configure({ showSpinner: false, speed: 100 });
+
 /**
  * A wrapper component that composes all client-side context providers.
  * This keeps the root layout as a Server Component and centralizes provider logic.
  */
 export const Providers = ({ children, messages, locale }: Props) => {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    if (NProgress.isStarted()) {
+      NProgress.done();
+    }
+  }, [pathname]);
+
   console.log(process.env.NEXT_PUBLIC_APP_VERSION);
   return (
     <ThemeRegistry>
--- src/components/Navigation/NavItem.tsx
+++ src/components/Navigation/NavItem.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { ComponentProps } from 'react';
+import NProgress from 'nprogress';
 
 import { Link, usePathname } from '@/lib/navigation';
 import { ROUTES } from '@/utils/constants';
@@ -28,6 +29,10 @@ export default function NavItem({
     ? pathname === ROUTES.HOME || activePaths.some(path => pathname === path)
     : pathname === href || activePaths.some(path => pathname.startsWith(path));
 
+  const handleClick = () => {
+    NProgress.start();
+  };
+
   return (
     <Link
       href={href}
@@ -36,6 +41,8 @@ export default function NavItem({
         isActive && 'active',
         'leading-none group flex flex-1 flex-col items-center justify-center text-11 font-normal text-gray-50 transition-colors duration-300 hover:text-primary active:text-primary'
       )}
+      onClick={handleClick}
+      prefetch={true}
     >
       <span className="mb-2 transition-all duration-300 group-hover:-translate-y-2">
         {isActive && activeIcon ? activeIcon : icon}
